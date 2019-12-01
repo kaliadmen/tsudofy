270618117107baa5320f4d979fb834b2f7badaae
BWorthy <kaliadmen@gmail.com>
2019-12-01 14:15:39 -0500

    Adds register form

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -19,7 +23,41 @@
                 <label for="password-login">Password</label>
                 <input id="password-login" name="password-login" type="password" required>
             </p>
-            <button type="submit" name="login-button">Login</button>
+            <button type="submit" name="login-button">LOGIN</button>
+        </form>
+
+        <form id="register-form" action=""method="POST">
+            <h2>Create your free account</h2>
+            <p>
+                <label for="username-register">Username</label>
+                <input id="username-register" name="username-register" type="text" placeholder="Enter a username" required>
+            </p>
+            <p>
+                <label for="first-name">First name</label>
+                <input id="first-name" name="first-name" type="text" placeholder="Enter your first name" required>
+            </p>
+            <p>
+                <label for="last-name">Last name</label>
+                <input id="last-name" name="last-name" type="text" placeholder="Enter your last name" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="Enter your email" required>
+            </p>
+            <p>
+                <label for="email-confirm">Confirm email</label>
+                <input id="email-confirm" name="email-confirm" type="email" placeholder="Reenter you email" required>
+            </p>
+            <p>
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" required>
+            </p>
+            <p>
+                <label for="password-confirm">Confirm password</label>
+                <input id="password-confirm" name="password-confirm" type="password" required>
+            </p>
+
+            <button type="submit" name="register-button">SIGN UP</button>
         </form>
     </div>
 </body>
